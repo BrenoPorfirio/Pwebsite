883c79f73a634b90f9c1e0eddaa8c201b3406c32
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-12-01 21:25:33 -0300

    fix id and add styles

--- pages/recipe16/onemovie/[id].js
+++ pages/recipe16/onemovie/[id].js
@@ -1,49 +1,57 @@
-import { useRouter } from "next/router"
-import useSWR from "swr"
-import { Button, Descriptions, Spin } from "antd"
+import { useRouter } from "next/router";
+import useSWR from "swr";
+import styles from './styles.module.css';
 
 export default function MovieDetail() {
-    const router = useRouter()
-    const { id } = router.query
+    const router = useRouter();
+    const { id } = router.query;
 
     const { data, error } = useSWR(
         id ? `https://www.omdbapi.com/?apikey=ca98445&i=${id}` : null,
         fetcher
-    )
+    );
 
     const handleReturnClick = () => {
-        router.push("/recipe16/movies3")
-    }
+        router.push("/recipe16/movies3");
+    };
 
     const ButtonReturn = ({ onClick }) => {
-        return <Button onClick={onClick}>Voltar</Button>
-    }
+        return <button className={styles['button-back']} onClick={onClick}>Voltar</button>;
+    };
 
-    if (error) return <div>Falha na requisição...</div>
-    if (!data) return <Spin tip="Carregando..." />
+    if (error) return <div className={styles['error-message']}>Falha na requisição...</div>;
+    if (!data) return <div className={styles['loading-message']}>Carregando...</div>;
 
     return (
-        <div style={{ padding: 20 }}>
+        <div className={styles['movie-detail-container']}>
             <ButtonReturn onClick={handleReturnClick} />
-            <div style={{ textAlign: "center", marginBottom: 20 }}>
-                <img src={data.Poster} alt={data.Title} style={{ maxWidth: "100%" }} />
+            <div className={styles['movie-detail-image']}>
+                <img src={data.Poster} alt={data.Title} />
             </div>
-            <h1 style={{ textAlign: "center" }}>{data.Title}</h1>
-            <Descriptions bordered column={1}>
-                <Descriptions.Item label="Ano de Lançamento">
-                    {data.Year}
-                </Descriptions.Item>
-                <Descriptions.Item label="Tipo">{data.Type}</Descriptions.Item>
-                <Descriptions.Item label="Duração">{data.Runtime}</Descriptions.Item>
-                <Descriptions.Item label="Gênero">{data.Genre}</Descriptions.Item>
-                <Descriptions.Item label="Enredo">{data.Plot}</Descriptions.Item>
-            </Descriptions>
+            <h1 className={styles['movie-detail-title']}>{data.Title}</h1>
+            <div className={styles['movie-detail-descriptions']}>
+                <p>
+                    <strong>Ano de Lançamento:</strong> {data.Year}
+                </p>
+                <p>
+                    <strong>Tipo:</strong> {data.Type}
+                </p>
+                <p>
+                    <strong>Duração:</strong> {data.Runtime}
+                </p>
+                <p>
+                    <strong>Gênero:</strong> {data.Genre}
+                </p>
+                <p>
+                    <strong>Enredo:</strong> {data.Plot}
+                </p>
+            </div>
         </div>
-    )
+    );
 }
 
 async function fetcher(url) {
-    const res = await fetch(url)
-    const json = await res.json()
-    return json
+    const res = await fetch(url);
+    const json = await res.json();
+    return json;
 }
